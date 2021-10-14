0feb3800782c9279152a764e41d3f1bb8e2d1b82
Dany <Andres@DESKTOP-4BN379M>
2021-10-13 23:53:50 -0300

    clase 0826

---
+++ SistemaBuscador/Repositories/LoginRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaBuscador.Repositories
+{
+    public class LoginRepository
+    {
+        public bool UserExit(string usuario, string password)
+        {
+            bool result = false;
+            string connectionString = "server=localhost;database=PRO402BD;Integrated Security=true;";
+            using SqlConnection sql = new SqlConnection(connectionString);
+            using SqlCommand cmd = new SqlCommand("sp_check_user", sql);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@user", usuario));
+            cmd.Parameters.Add(new SqlParameter("@password", password));
+            sql.Open();
+            int bdResult = (int)cmd.ExecuteScalar();
+            if (bdResult > 0)
+            {
+                result = true;
+            }
+
+
+
+
+            return result;
+        }
+
+        internal bool UserExist(string usuario, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool UserExist(string usuario, object password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
